f8c4bbf8a4c7e0efa9fcb76da89fe0956b2d0d2c
Tim Kelly <63365669+timotheekelly@users.noreply.github.com>
2023-12-06 16:57:18 +0000

    Utilize ObjectId

--- src/main/java/com/example/azureblob/models/ImageMetadata.java
+++ src/main/java/com/example/azureblob/models/ImageMetadata.java
@@ -2,20 +2,21 @@ package com.example.azureblob.models;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.bson.types.ObjectId;
 
 @Document(collection = "imageMetadata")
 public class ImageMetadata {
 
     @Id
-    private String id;
+    private ObjectId id;
     private String caption;
     private String imageUrl;
 
-	public String getId() {
+	public ObjectId getId() {
 		return id;
 	}
 
-	public void setId(String id) {
+	public void setId(ObjectId id) {
 		this.id = id;
 	}
 
